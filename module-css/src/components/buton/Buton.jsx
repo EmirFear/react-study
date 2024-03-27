bfb2e6c15e11182b787cfd9308fee8bc0fa4b8b6
Emir <emir.ser32@gmail.com>
2024-03-27 16:04:14 +0300

    finish

--- module-css/src/components/buton/Buton.jsx
+++ module-css/src/components/buton/Buton.jsx
@@ -2,13 +2,24 @@ import React from "react";
 // import "./Buton.css";
 import ButonStyle from "./Buton.module.css";
 
+//* Eğer CSS'deki class adi kebap-case seklinde yazilmis ise
+//* module ile bu class'a erismek icin ornekte goruldugu gibi
+//* koseli parentez ile erisim yapilir.Koseli parentez erisimi
+//* en garanti yontemdir.
+
+//* ORN:moduleName["class-adi"]
+
 const Buton = () => {
   return (
-    <div className='wrapper'>
-        <button className='btn btn-blue'>Info</button>
-        <button className='btn btn-red'>Back</button>
+    <div className={ButonStyle.wrapper}>
+      <button className={`${ButonStyle.btn} ${ButonStyle["btn-blue"]}`}>
+        Info
+      </button>
+      <button className={`${ButonStyle.btn} ${ButonStyle["btn-red"]}`}>
+        Back
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Buton
+export default Buton;
--- module-css/src/components/card/Card.jsx
+++ module-css/src/components/card/Card.jsx
@@ -1,14 +1,22 @@
 import Buton from "../buton/Buton";
-// import "./Card.css"
-import Cardstyle from "./Card.module.css"
+// import "./Card.css";
+import CardStyle from "./Card.module.css";
+
+//* module.css'de derleyici CSS class'larini alarak bunlari unique olacak
+//* sekilde yeniden adlandirir. (Ornek: card_title__XaSde)
+//* Bu durumda, global scope problemi ortadan kalktigi icin stillerin
+//* baska class tarafindan ezilmesi (overriding) engellenir.
+//* CCS module Webpack, Browsify gibi tool'lar ile kullanilabilir.
 
 const Card = ({ lang, img }) => {
   return (
     <div>
-      <div className={wrapper>
-        <h1> {lang} </h1>
+      {/* <div className="wrapper"> */}
+      <div className={CardStyle.wrapper}>
+        <h1>{lang} </h1>
       </div>
-      <img className="images" src={img} alt={lang} />
+      {/* <img className="images" src={img} alt={lang} /> */}
+      <img className={CardStyle.images} src={img} alt={lang} />
       <Buton />
     </div>
   );
